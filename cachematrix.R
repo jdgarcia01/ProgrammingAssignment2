8d74b8c772db331deed24d29209999fb05fa49cc
John Garcia <john.d.garcia01@gmail.com>
2016-05-30 13:27:31 -0500

    Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,66 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Assignment 2 for Coursera Data Science R programming.
+## This assignment is used to illistrate the use 
+## of R caching operator <<-  
 
-## Write a short comment describing this function
-
+## This function will 
+## 1.) set the value of a matrix
+## 2.) get the value of a matrix
+## 3.) store (cache) the inverse value of a matrix.
+## 3.) get the (cached) inverse value of a matrix.  
 makeCacheMatrix <- function(x = matrix()) {
 
+  mat_inv <- NULL
+  
+  set <- function(y) {
+    x <<- y
+    mat_inv <<- NULL
+  }
+  
+  get <- function() x
+  
+  set_inverse <- function(inverse) mat_inv <<- inverse
+  get_inverse <- function() mat_inv
+  
+  list(set=set, get=get, set_inverse=set_inverse, get_inverse=get_inverse)
+  
+  
 }
 
 
-## Write a short comment describing this function
+## This function will get the inverse of a matrix. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  ## Do we already have the inverse of the matrix? 
+  mat_inv <- x$get_inverse()
+  if(!is.null(mat_inv)) {
+    message("getting cached data.")
+    return(mat_inv)
+    
+  } else {
+    ## We don't, so determine what the inverse should be.  
+    data <- x$get()
+    inv <- solve(data)
+    x$set_inverse(inv)
+    inv
+    
+  }    
+  
 }
+
+# Test case
+# x <- rbind( c(2,4), c(6,8))
+# m <- matrixCacheMatrix(x)
+# cacheSolve(m)
+#     [1,]      [,2]
+#[1,]   -1.00   0.50
+#[2,]    0.75  -0.25
+#
+# cacheSolve(m)
+# getting cached data.
+#     [1,]      [,2]
+#[1,]   -1.00   0.50
+#[2,]    0.75  -0.25
+
+
+
